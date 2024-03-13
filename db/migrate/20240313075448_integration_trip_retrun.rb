8b7c65409a384c52f0ae6d120e4b56f0c24afba5
haga_hiroki <ragna.1123@gmail.com>
2024-03-13 17:12:03 +0900

    trip_returnの統合

---
+++ db/migrate/20240313075448_integration_trip_retrun.rb
@@ -0,0 +1,10 @@
+class IntegrationTripRetrun < ActiveRecord::Migration[7.1]
+  def change
+    # trips_retrunテーブルを削除
+    drop_table :trip_returns
+
+    # tripsテーブルにカラムを追加
+    add_column :trips, :return_time, :datetime
+    add_column :trips, :return_details, :text
+  end
+end
