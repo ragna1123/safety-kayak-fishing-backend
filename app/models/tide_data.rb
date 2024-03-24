e433891425289edda024ea4e3e495ab58e3b8ff6
haga_hiroki <ragna.1123@gmail.com>
2024-03-25 08:13:16 +0900

    潮位データの記録を実装

--- app/models/tide_data.rb
+++ app/models/tide_data.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class TideData < ApplicationRecord
-  belongs_to :location
-
   # バリデーション
   validates :time, presence: true
   validates :tide_type, presence: true
   validates :height, presence: true
+
+  belongs_to :location
+
+  # tide_data テーブルと trips テーブルの多対多の関連を設定
+  has_many :trip_tides
+  has_many :trips, through: :trip_tides
+
 end
--- app/workers/tide_recording_worker.rb
+++ app/workers/tide_recording_worker.rb
@@ -1,21 +1,37 @@
-# frozen_string_literal: true
-
 class TideRecordingWorker
   include Sidekiq::Worker
 
-  def perform(location_id)
-    location = Location.find(location_id)
+  TIME_ZONE = 'Tokyo'
 
-    # StormglassIdTideService などを使って潮位データを取得
-    tide_service = StormglassIoTideService.new(ENV['STORM_GLASS_API_KEY'])
-    tide_data = tide_service.fetch_tide_data(location.latitude, location.longitude, Time.now, Time.now + 1.hour)
-
-    # 取得した潮位データをデータベースに保存
-    Tide.create!(
-      location:,
-      recorded_at: Time.now,
-      tide_level: tide_data['level']
-      # 他の必要なフィールドがあればここに追加
-    )
+  def perform(trip_id)
+    trip = Trip.find_by(id: trip_id)
+    return unless trip
+  
+    location = trip.location
+    start_time = trip.departure_time.in_time_zone(TIME_ZONE).beginning_of_day
+    end_time = trip.departure_time.in_time_zone(TIME_ZONE).end_of_day
+  
+    tide_service = StormGlassIoTideService.new
+    response = tide_service.fetch_tide_data(location.latitude, location.longitude, start_time, end_time)
+  
+    # レスポンスの構造に応じて適切なデータを取得
+    tide_data = response["data"]
+  
+    tide_data.each do |data|
+      puts data.inspect
+      
+      tide = TideData.create!(
+        location: location,
+        time: data['time'],
+        tide_type: data['type'],
+        height: data['height']
+      )
+    
+      TripTide.create!(
+        trip_id: trip.id,
+        tide_data_id: tide.id
+      )
+    end
+    
   end
 end
