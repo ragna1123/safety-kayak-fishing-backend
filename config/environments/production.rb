a4fff518966fb8090cf7eafed3eecf56454eaf5d
hiroki haga <hirokihaga@hirokinoMacBook-Pro.local>
2024-07-02 18:47:51 +0900

    productionの設定見直し

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  
-  # SSL無効化
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
